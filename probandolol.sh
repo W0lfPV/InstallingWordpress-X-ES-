96ac29ffd4bb3022007c85629987985a7123b942
Adrián Diaz <freeadrike@gmail.com>
2017-02-03 18:36:35 +0100

    Add files via upload

---
+++ probandolol.sh
@@ -0,0 +1,14 @@
+echo "Se va a proceder a descargar la última versión de Wordpress"
+wget https://wordpress.org/latest.zip
+echo "Moviendo el archivo descargado a /var/www/html"
+mv latest.zip /var/www/html
+cd /var/www/html
+unzip latest.zip
+carpeta=$(zenity --entry --title "Configúrelo a su gusto" --text "¿Qué nombre quiere ponerle al directorio de Wordpress en el servidor? Al añadir localhost/**** como URL podrá acceder a Wordpress")
+mv wordpress $carpeta
+echo $carpeta creado
+chown www-data:www-data $carpeta -R
+echo $carpeta configurado
+zenity --info --title "Todo correcto." --text "Creado directorio de Wordpress /var/www/html/$carpeta , configurado según sus especificaciones."
+exit
+
