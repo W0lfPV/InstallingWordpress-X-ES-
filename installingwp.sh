267959e42bc9e65faf4994a02bacd7a12853d31f
Adrián Diaz <freeadrike@gmail.com>
2017-02-08 03:05:05 +0100

    Update installingwp.sh

--- installingwp.sh
+++ installingwp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Se va a proceder a descargar la última versión de Wordpress"
 wget https://wordpress.org/latest.zip
+echo "Se debe instalar lo necesario"
+apt-get install apache2 mysql-server mysql-client zenity
+echo "Se debe haber instalado con normalidad"
 echo "Moviendo el archivo descargado a /var/www/html"
 mv latest.zip /var/www/html
 cd /var/www/html
@@ -11,6 +14,15 @@ echo $carpeta creado
 chown www-data:www-data $carpeta -R
 echo $carpeta configurado
 rm latest.zip
+cd 
+touch sql.txt
+basededatos=$(zenity --entry --title "Creando base de datos" --text "¿Qué nombre quiere ponerle a la base de datos?")
+usuariobase=$(zenity --entry --title "Creando usuario de base de datos" --text "¿Qué nombre quiere ponerle al usuario en la base de datos?")
+passuserbase=$(zenity --entry --title "Creando contraseña para usuario" --text "¿Qué contraseña quiere ponerle al usuario de la base de datos?")
+echo "CREATE DATABASE $basededatos;" > sql.txt
+echo "GRANT ALL PRIVILEGES ON $basededatos.* TO '$usuariobase'@'localhost' IDENTIFIED BY '$passuserbase';" >> sql.txt
+zenity --info --title "Procesamos a la creación de la base de datos" --text "A continuación le pedirá la contraseña del usuario "root" de su base de datos, introduzcala y pulse Enter, si nunca la ha cambiado es posible que sea 123."
+mysql -u root -p < sql.txt
+rm sql.txt
 zenity --info --title "Todo correcto." --text "Creado directorio de Wordpress /var/www/html/$carpeta , configurado según sus especificaciones."
 exit
-
