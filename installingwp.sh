c5debf671334b6c7068cefd8450fefc374585829
Adrián Diaz <freeadrike@gmail.com>
2017-02-08 03:20:31 +0100

    Update installingwp.sh

--- installingwp.sh
+++ installingwp.sh
@@ -18,11 +18,15 @@ cd
 touch sql.txt
 basededatos=$(zenity --entry --title "Creando base de datos" --text "¿Qué nombre quiere ponerle a la base de datos?")
 usuariobase=$(zenity --entry --title "Creando usuario de base de datos" --text "¿Qué nombre quiere ponerle al usuario en la base de datos?")
-passuserbase=$(zenity --entry --title "Creando contraseña para usuario" --text "¿Qué contraseña quiere ponerle al usuario de la base de datos?")
+passuserbase=$(zenity --entry --title "Creando contraseña para usuario" --text "¿Qué contraseña quiere ponerle al usuario de la base de datos?" --hide-text)
 echo "CREATE DATABASE $basededatos;" > sql.txt
 echo "GRANT ALL PRIVILEGES ON $basededatos.* TO '$usuariobase'@'localhost' IDENTIFIED BY '$passuserbase';" >> sql.txt
 zenity --info --title "Procesamos a la creación de la base de datos" --text "A continuación le pedirá la contraseña del usuario "root" de su base de datos, introduzcala y pulse Enter, si nunca la ha cambiado es posible que sea 123."
 mysql -u root -p < sql.txt
+echo "Base de datos creada"
+echo "Usuario mysql creado"
+echo "Establecida contraseña al nuevo usuario"
+echo "Hemos terminado :-)"
 rm sql.txt
-zenity --info --title "Todo correcto." --text "Creado directorio de Wordpress /var/www/html/$carpeta , configurado según sus especificaciones."
+zenity --info --title "Todo correcto." --text "Ya puede ir al navegador y entrar a localhost/$carpeta para ir a WordPress, no olvide los datos sobre la base de datos para añadirlos una vez dentro del proceso de configuración vía web de WordPress, la base de datos se llama $basededatos y el usuario $usuariobase"
 exit
